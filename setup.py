b1771955c6b2629e27bcf1e14a9d7ce09fe1f651
R. Lindsay Vanderbeck <lindsay.vanderbeck@live.ca>
2021-01-01 13:01:32 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="example-pkg-vanderbeck", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    author="Lindsay Vanderbeck",
+    author_email="lindsay.vanderbeck@live.ca",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Vanderbeck/example_pkg.git",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'numpy',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
